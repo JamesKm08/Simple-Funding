5e498570ed55f31c7d68436e8a496220776cba79
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-06-12 14:42:30 +0300

    Update fund.sol

--- fund.sol
+++ fund.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 < 0.9.0;
 
+contract Fund{
+
+//pinpointing an integer to the address needed to be funded
+    mapping (address => uint256) public AddressFunded;
+
+//create a function with keyword payable to enable the funding transaction to work
+    function fund() public payable {
+        AddressFunded[msg.sender] += msg.value;
+        
+    }
+
+}
